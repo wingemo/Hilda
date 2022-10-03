300b25986674dbdf671fa838b6eb9eefb6f28032
Wingemo <philip.wingemo@outlook.com>
2022-10-03 13:15:11 +0200

    Update user.js

--- Users/model/user.js
+++ Users/model/user.js
@@ -1,1 +1,14 @@
+function select(p1, p2) {
+  var sql = "SELECT * FROM students";
+  return connection.query(sql);
+};
 
+function delete(p1, p2) {
+  var sql = " * FROM students";
+  return connection.query(sql);
+};
+
+function update(p1, p2) {
+  var sql = " * FROM students";
+  return connection.query(sql);
+};
